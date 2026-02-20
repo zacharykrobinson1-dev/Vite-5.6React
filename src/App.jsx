fad158a3f4122511badf0e333b7f876ae14ede99
zacharykrobinson1-dev <zacharykrobinson1@gmail.com>
2026-02-20 17:09:12 +0000

    try

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+{/* MagicButton component from video */
 function MagicButton() {
 
   return (
@@ -12,6 +14,63 @@ function MagicButton() {
   )
 
 }
+{/* ---------------COMPONENET #1-------------------- */
+
+
+{/* ---------------- DATA STRUCTURE ---------------- */
+
+
+const books = [
+  {
+    id: 1,
+    title: "A Farewell to Arms",
+    author: "Ernest Hemingway",
+    year: 1929
+  },
+  {
+    id: 2,
+    title: "1984",
+    author: "George Orwell",
+    year: 1949
+  },
+  {
+    id: 3,
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    year: 1925
+  }
+]
+
+/* -------- COMPONENT #2 -------- */
+function BookCard({ title, author, year }) {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <p>Author: {author}</p>
+      <p>Published: {year}</p>
+    </div>
+  )
+}
+
+/* -------- COMPONENT #3 -------- */
+function BookList({ bookArray }) {
+  return (
+    <div>
+      <h2>Book List</h2>
+      {bookArray.map((book) => (
+        <BookCard
+          key={book.id}
+          title={book.title}
+          author={book.author}
+          year={book.year}
+        />
+      ))}
+    </div>
+  )
+}
+
+
+
 
 
 function App() {
@@ -41,6 +100,7 @@ function App() {
         My name is zachary robinson and this is my change
       </p>
       <MagicButton />
+      <BookList bookArray={books} />
     </>
   )
 }
